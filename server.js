d667db99ed54075a18314c3f0f6459a82befc94e
tomdaniels <thomsondaniels@hotmail.com>
2018-06-13 12:28:51 +1000

    refactor: server.js

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-
 require('./src/routes')(app, {});
 
 app.listen(port, () => {
